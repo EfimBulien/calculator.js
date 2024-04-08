da473a43b51db5878d8392c6d239233fd1250a75
EfimBulien <isip_e.m.bulien@mpt.ru>
2024-04-08 19:20:20 +0300

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const display = document.getElementsByClassName("numbers");
+const display = document.getElementById("display")
 
-function displaynumbers(input){
-    display.value += input;
+function displaySymbol(input) {
+    try {
+        display.value += input;
+    } catch(error) {
+        display.value = input;
+    }
+}
+
+function calculate(){
+    try{
+        const sanitizedInput = display.value.replace(/[^\d.+*/()-]/g, "");
+        display.value = eval(sanitizedInput);
+    }
+    catch(error) {
+        clearDisplay();
+    }
+}
+
+function clearDisplay() {
+    display.value = "";
 }
